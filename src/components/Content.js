49257dc240240579d7b3ff8d3b94612ab337b2d1
waqas saleem <waqas.mba86@gmail.com>
2021-06-10 14:10:16 +0500

    now started to create an app of ledgers record

--- src/components/Content.js
+++ src/components/Content.js
@@ -7,12 +7,12 @@ import firebase from './Fire'
       constructor(){
           super();
           this.state = {
-            itemName:'',
-            costPrice:'',
+            // itemName:'',
+            // costPrice:'',
             partyName:'',
-            partyContact:'',
-            ownerName:'',
-            message:'',    
+            // partyContact:'',
+            // ownerName:'',
+            // message:'',    
           }
       }
 
@@ -29,22 +29,22 @@ this.setState({
 
 
 
-saveItem = ()=> {
-  let obj = {};
-  obj.itemName = this.state.itemName;
-  // obj.costPrice = Number(this.state.costPrice);     Commented Now
+// saveItem = ()=> {
+//   let obj = {};
+//   obj.itemName = this.state.itemName;
+//   // obj.costPrice = Number(this.state.costPrice);     Commented Now
   
 
 
-  var key = firebase.database().ref('itemList').push().key
-  obj.key = key
-  firebase.database().ref('itemList').child(key).set(obj)
-  alert('saved successfully')
- this.setState({itemName:'', costPrice:'',age:''}) 
+//   var key = firebase.database().ref('itemList').push().key
+//   obj.key = key
+//   firebase.database().ref('itemList').child(key).set(obj)
+//   alert('saved successfully')
+//  this.setState({itemName:'', costPrice:'',age:''}) 
 
- console.log(obj)
+//  console.log(obj)
 
-}
+// }
 
 
 
@@ -55,8 +55,8 @@ saveItem = ()=> {
 saveParty = ()=> {
   let partyObj = {};
   partyObj.partyName = this.state.partyName;
-  partyObj.partyContact = this.state.partyContact;
-  partyObj.ownerName = this.state.ownerName;
+  // partyObj.partyContact = this.state.partyContact;
+  // partyObj.ownerName = this.state.ownerName;
   // obj.costPrice = Number(this.state.costPrice);
   // obj.age = this.state.age;
   // Temporily coment
@@ -66,7 +66,7 @@ saveParty = ()=> {
   partyObj.key = key
   firebase.database().ref('partyList').child(key).set(partyObj)
   alert('saved successfully')
- this.setState({itemName:'', costPrice:'',partyName:'', ownerName:'',partyContact:''}) 
+ this.setState({partyName:''}) 
 
  console.log(partyObj)
 
@@ -92,12 +92,12 @@ saveParty = ()=> {
     <div>
     {/* Add Item Name */}
     <br/><br/><br/>
-    <h2 className='headings'>Add Item Name</h2>
-    <input type='text'  value={this.state.itemName} name='itemName' onChange={this.changeHandler} placeholder='Add Your Item Name with Brand' />  <br/>
+    {/* <h2 className='headings'>Add Item Name</h2> */}
+    {/* <input type='text'  value={this.state.itemName} name='itemName' onChange={this.changeHandler} placeholder='Add Your Item Name with Brand' />  <br/> */}
     {/* <input type='text' value={this.state.costPrice} name='costPrice' onChange={this.changeHandler} placeholder='cost Price' /> <br/>    Commented Now */}
     {/* <input type='text' value={this.state.age} name='age' onChange={this.changeHandler} placeholder='Age' /><br/>  */}
     {/* <button onClick={this.save}>Save</button> */}
-    <button className="waves-effect waves-dark btn" onClick={this.saveItem}>Save</button>
+    {/* <button className="waves-effect waves-dark btn" onClick={this.saveItem}>Save</button> */}
 
 
 
@@ -108,10 +108,10 @@ saveParty = ()=> {
 
 {/* Add Party */}
 <br/><br/><br/>
-    <h2 className='headings'>Add Party Name</h2>
-    <input type='text'  value={this.state.partyName} name='partyName' onChange={this.changeHandler} placeholder='Add Your Party Name' />  <br/>
-    <input type='text' value={this.state.partyContact} name='partyContact' onChange={this.changeHandler} placeholder='Contact Details' /> <br/>
-    <input type='text' value={this.state.ownerName} name='ownerName' onChange={this.changeHandler} placeholder='ownerName' /><br/> 
+    <h2 className='headings'>Create Account</h2>
+    <input type='text'  value={this.state.partyName} name='partyName' onChange={this.changeHandler} placeholder='Account Title' />  <br/>
+    {/* <input type='text' value={this.state.partyContact} name='partyContact' onChange={this.changeHandler} placeholder='Contact Details' /> <br/> */}
+    {/* <input type='text' value={this.state.ownerName} name='ownerName' onChange={this.changeHandler} placeholder='ownerName' /><br/>  */}
     {/* <button onClick={this.save}>Save</button> */}
     <button className="waves-effect waves-dark btn" onClick={this.saveParty}>Save</button>
 
--- src/components/Fire.js
+++ src/components/Fire.js
@@ -4,13 +4,13 @@ import 'firebase/database'
 
 
 var firebaseConfig = {
-    apiKey: "AIzaSyCRKceXGhdhpKFQiAJ2Qgkb1wXgXbI36RQ",
-    authDomain: "ngrpractice.firebaseapp.com",
-    projectId: "ngrpractice",
-    storageBucket: "ngrpractice.appspot.com",
-    messagingSenderId: "384804225359",
-    appId: "1:384804225359:web:d1c84f17b1de731c99853a",
-    measurementId: "G-NKKRC8FGHL"
+  apiKey: "AIzaSyCri9N8uwpTpBFmr5RGITpH1tUrlLjMKBk",
+  authDomain: "myledgers-5c8bc.firebaseapp.com",
+  projectId: "myledgers-5c8bc",
+  storageBucket: "myledgers-5c8bc.appspot.com",
+  messagingSenderId: "370025177031",
+  appId: "1:370025177031:web:832c3f9a032226e9cd83e2",
+  measurementId: "G-L26JFRZLKN"
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
@@ -18,3 +18,16 @@ var firebaseConfig = {
 
 
 export default firebase;
+
+
+
+
+
+
+    // apiKey: "AIzaSyCRKceXGhdhpKFQiAJ2Qgkb1wXgXbI36RQ",
+    // authDomain: "ngrpractice.firebaseapp.com",
+    // projectId: "ngrpractice",
+    // storageBucket: "ngrpractice.appspot.com",
+    // messagingSenderId: "384804225359",
+    // appId: "1:384804225359:web:d1c84f17b1de731c99853a",
+    // measurementId: "G-NKKRC8FGHL"
