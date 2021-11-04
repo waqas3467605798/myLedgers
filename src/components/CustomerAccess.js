0b1893b29267ab9387b1dd7dd515d0c78ce81bad
waqas saleem <waqas.mba86@gmail.com>
2021-11-04 19:57:31 +0500

    working on customer access

--- src/components/CustomerAccess.js
+++ src/components/CustomerAccess.js
@@ -13,9 +13,13 @@ class CustomerAccess extends Component{
         this.state ={
             customerAccessList:[],
             uid:'',
+            partiesObjects:[],
             keyWords:'',
             ledger:[],
-            ledgerBalance:[]
+            ledgerBalance:[],
+            ourObj:[],
+            ledgerFor500trans:-500,
+            
         }
 
     }
@@ -55,40 +59,55 @@ class CustomerAccess extends Component{
 
 
 
-    showStmnt=()=>{
-
-    var uidPromise = new Promise((res,rej)=>{
-        var reqObj = this.state.customerAccessList.find( (o)=>{return o.keyWords === this.state.keyWords}  )
-    res(reqObj)
-    rej('Operation faild')
-    })
-    uidPromise.then( (rqObj)=>{
-        var objectsPromise = new Promise(  (res,rej)=>{ 
-            var partyObjects=[]
-            firebase.database().ref('partyList'+rqObj.uid).on('child_added' , (data)=> { 
-                partyObjects.push(data.val())
-              }  )
-        res(partyObjects)
-        rej('operation faild')
-            }  )
-            objectsPromise.then( (objts)=>{
-                var ourObjectPromise = new Promise( (res,rej)=>{
-                var displayObj = objts.find((o)=>{return o.keyWords === this.state.keyWords})
-            res(displayObj)
-            rej('operation failed')
-            } ) //from here dot then will be run
-            ourObjectPromise.then( (oo)=>{this.setState({ledger:oo.ledger, ledgerBalance:oo.sum})},(er)=>{} )
-                
-            }, (errr)=>{}  )
-        
-}, (err)=>{}  )
-
-
-
-
-
-    }
     
+
+
+showStmnt=()=>{
+    
+    var reqObj = this.state.customerAccessList.find( (o)=>{return o.keyWords === this.state.keyWords}  )
+
+    if(reqObj){
+
+
+var myPromise = new Promise(  (res,rej)=>{  
+    var reqObj = this.state.customerAccessList.find( (o)=>{return o.keyWords === this.state.keyWords}  )
+     res(reqObj)
+     rej('Operation faild')
+
+
+
+     })
+ 
+myPromise
+.then( (obj)=>{  
+
+    firebase.database().ref('partyList'+obj.uid).on('child_added' , (data)=> { 
+        this.state.partiesObjects.push(data.val())
+        }  ) 
+
+ } )
+.then( ()=>{  
+
+    var ourObj = this.state.partiesObjects.find( (ob)=>{return ob.keyWords === this.state.keyWords}  )
+this.setState({ourObj:ourObj})
+ } )
+.then( ()=>{  
+   this.setState({ledger:this.state.ourObj.ledger, ledgerBalance:this.state.ourObj.sum})
+ } )
+// .then( ()=>{   } )
+
+
+
+
+    }else{
+        alert('Your Entered Key Words is not Correct')
+    }
+
+
+}
+
+
+
 
 
 
@@ -97,15 +116,19 @@ class CustomerAccess extends Component{
 
     render(){
         return(
-        <div>
+        <div id='up'>
 
 <input type='text' name='keyWords' onChange={this.changeHandler} placeholder='write key words here' />
 <button className="waves-effect waves-dark btn" onClick={this.showStmnt} >Show Statement</button>
 
 
-<table style={{maxWidth:'950px',margin:'auto'}}><thead><tr><th style={{textAlign:'center'}}>V#</th><th>Date</th><th>Remarks</th><th>Debit</th><th>Credit</th><th>Balance</th><td><a href='#down' style={{color:'blue'}} className="tiny material-icons">arrow_downward</a></td></tr></thead><tbody>{this.state.ledger.map(  (item,index)=>{return <tr key={index}><td style={{color:'blue', textAlign:'center'}}>{item.voucherNumber}</td><td>{item.date}</td><td style={{maxWidth:'135px',color:'blue'}}>{item.narration}</td><td>{item.debit >=0 ? item.debit : ''}</td><td style={{color:'blue'}}>{item.debit <0 ? item.debit : ''}</td><td className={this.state.ledgerBalance.slice(0,index+2).reduce( (total,num)=>{return total+num},0) >= 0 ? 'ldgrPostveAmt' : 'ldgrNegtveAmt'}><b>{this.state.ledgerBalance.slice(0,index+2).reduce( (total,num)=>{return total+num},0)}</b></td><td><a href='#' style={{fontSize:'16px', color:'red'}} className="material-icons" onClick={()=>this.deleteLedgerEntry(index)}>delete</a><a href='#' style={{fontSize:'16px', color:'green'}} className="small material-icons" onClick={()=> this.editEntry(index)}>edit</a></td></tr>}).slice(this.state.ledgerFor30Days)  }<tr><td></td><td></td><td><b>TOTAL</b></td><td><b>{this.state.debitTotal}</b></td><td><b>{this.state.creditTotal}</b></td><td style={{fontSize:'12px',color:'blue'}}><b>CL. BAL <i className="tiny material-icons">arrow_upward</i></b></td><td><a href='#up' style={{color:'blue'}} className="tiny material-icons">arrow_upward</a></td></tr></tbody></table>
+<p style={{color:'red',fontSize:'20px'}}>Last 500-Transactions <br/>
+   Account Title: <span style={{color:'green'}}> {this.state.ourObj.partyName} </span></p>
+
+<table style={{maxWidth:'950px',margin:'auto'}}><thead><tr><th style={{textAlign:'center'}}>V#</th><th>Date</th><th>Remarks</th><th>Debit</th><th>Credit</th><th>Balance</th><td><a href='#down' style={{color:'blue'}} className="tiny material-icons">arrow_downward</a></td></tr></thead><tbody>{this.state.ledger.map(  (item,index)=>{return <tr key={index}><td style={{color:'blue', textAlign:'center'}}>{item.voucherNumber}</td><td>{item.date}</td><td style={{maxWidth:'135px',color:'blue'}}>{item.narration}</td><td>{item.debit >=0 ? item.debit : ''}</td><td style={{color:'blue'}}>{item.debit <0 ? item.debit : ''}</td><td className={this.state.ledgerBalance.slice(0,index+2).reduce( (total,num)=>{return total+num},0) >= 0 ? 'ldgrPostveAmt' : 'ldgrNegtveAmt'}><b>{this.state.ledgerBalance.slice(0,index+2).reduce( (total,num)=>{return total+num},0)}</b></td></tr>}).slice(this.state.ledgerFor500trans)  }<tr><td></td><td></td><td><b>TOTAL</b></td><td><b>{this.state.debitTotal}</b></td><td><b>{this.state.creditTotal}</b></td><td style={{fontSize:'12px',color:'blue'}}><b>CL. BAL <i className="tiny material-icons">arrow_upward</i></b></td><td><a href='#up' style={{color:'blue'}} className="tiny material-icons">arrow_upward</a></td></tr></tbody></table>
 
 
+<p id='down'></p>
         </div>
         )
     }
