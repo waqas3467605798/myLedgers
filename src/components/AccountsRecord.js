86067527f5034e6880f5f21c9d7a108cd090f35d
waqas saleem <waqas.mba86@gmail.com>
2021-11-21 12:09:07 +0500

    Customer access is sucessfully completed

--- src/components/AccountsRecord.js
+++ src/components/AccountsRecord.js
@@ -40,7 +40,7 @@ import {Link, Route, BrowserRouter} from 'react-router-dom'
           <Route path='/AccountsRecord/Ledger' component={Ledger}/>
           <Route path='/AccountsRecord/Trial' component={Trial}/>
           <Route path='/AccountsRecord/Vouchers' component={Vouchers}/>
-          <Route path='/AccountsRecord/OnlineAccess' component={OnlineAccess}/>
+          {/* <Route path='/AccountsRecord/OnlineAccess' component={OnlineAccess}/> */}
           </div>
 
           </div>
@@ -75,7 +75,7 @@ class MainBar extends Component{
           <Link to='/AccountsRecord/Vouchers' style={{textDecoration:'none', marginRight:'22px', color:'red'}} > Vouchers </Link>
           <Link to='/AccountsRecord/Ledger' style={{textDecoration:'none', marginRight:'22px', color:'red'}} > Account Statement </Link>
           <Link to='/AccountsRecord/Trial' style={{textDecoration:'none', marginRight:'22px', color:'red'}} > Summary </Link>
-          <Link to='/AccountsRecord/OnlineAccess' style={{textDecoration:'none', marginRight:'22px', color:'red'}} > Access </Link>
+          {/* <Link to='/AccountsRecord/OnlineAccess' style={{textDecoration:'none', marginRight:'22px', color:'red'}} > Access </Link> */}
           
           </div>
   
@@ -1383,213 +1383,243 @@ viewVoucher = ()=>{
 
 
 
-  class OnlineAccess extends Component{
-    constructor(){
-      super();
-      this.state ={
-              user:null,
-              userEmail:null,
-              partyObjects:[],
-              status:false,
-              keyWords:'',
-              customerAccessList:[],
-              listStatus:false
-      }
+//   class OnlineAccess extends Component{
+//     constructor(){
+//       super();
+//       this.state ={
+//               user:null,
+//               userEmail:null,
+//               partyObjects:[],
+//               status:false,
+//               keyWords:'',
+//               customerAccessList:[],
+//               listStatus:false
+//       }
 
-  }
-
-
-  async componentDidMount(){
-    var dataPushPromise = new Promise( (res,rej)=>{
-    var userId = firebase.auth().currentUser.uid;
-    var userEmail = firebase.auth().currentUser.email
-    this.setState({user:userId,userEmail:userEmail})
-    res()
-    rej('Operation Failed: Data From Firebase does not push in state successfully')
-  } )
-  dataPushPromise.then(()=>{
-
-    var pushPromise = new Promise((resolve,reject)=>{
-      var obj = [];
-      firebase.database().ref('partyList'+this.state.user).on('child_added' , (data)=> { 
-        obj.push(data.val())
-      }  )
-      resolve(obj)
-      reject('Operation failed')
-    })
-    pushPromise.then((ob)=>{
-      this.setState({partyObjects:ob})
-    },(er)=>{
-      alert(er)
-    })
+//   }
 
 
-  },(err)=>{
-    alert(err)
-  })
+//   async componentDidMount(){
+//     var dataPushPromise = new Promise( (res,rej)=>{
+//     var userId = firebase.auth().currentUser.uid;
+//     var userEmail = firebase.auth().currentUser.email
+//     this.setState({user:userId,userEmail:userEmail})
+//     res()
+//     rej('Operation Failed: Data From Firebase does not push in state successfully')
+//   } )
+//   dataPushPromise.then(()=>{
+
+//     var pushPromise = new Promise((resolve,reject)=>{
+//       var obj = [];
+//       firebase.database().ref('partyList'+this.state.user).on('child_added' , (data)=> { 
+//         obj.push(data.val())
+//       }  )
+//       resolve(obj)
+//       reject('Operation failed')
+//     })
+//     pushPromise.then((ob)=>{
+//       this.setState({partyObjects:ob})
+//     },(er)=>{
+//       alert(er)
+//     })
 
 
-  firebase.database().ref('customerAccess').on('child_added' , (data)=> { 
-    this.state.customerAccessList.push(data.val())
-  }  )
+//   },(err)=>{
+//     alert(err)
+//   })
 
 
-
-}
+//   firebase.database().ref('customerAccess').on('child_added' , (data)=> { 
+//     this.state.customerAccessList.push(data.val())
+//   }  )
 
 
 
-getData =()=>{
-  this.setState({status: ! this.state.status})
-}
-
-
-
-changeHandler=(e)=>{
-this.setState({[e.target.name]: e.target.value  })
-
-
-}
-  
-
-allowAccess = ()=>{
-
-
- var alreadyPartyExist = this.state.customerAccessList.find(  (o)=>{return o.partyName === document.getElementById('selected_save2').value}  )
- var alreadyKeyExist = this.state.customerAccessList.find(  (o)=>{return o.keyWords === this.state.keyWords}  )
- 
- 
-if(alreadyPartyExist){
-  alert('Alread Access alowed to the selected Account')
-}else{
-
- 
-  if(alreadyKeyExist){
-    alert('Password already exist, Plz change your Password')
-  }else{
-
-
-var obj = []
-obj.keyWords = this.state.keyWords
-obj.partyName = document.getElementById('selected_save2').value
-obj.uid = this.state.user
-
-var accountTitle = document.getElementById('selected_save2').value
-var reqObj = this.state.partyObjects.find(  (ob)=>{return ob.partyName === accountTitle}  )
-reqObj.keyWords = this.state.keyWords
-// if('ledger' in reqObj){
-// obj.ledger = reqObj.ledger
-// obj.sum = reqObj.sum
-// }else{
-//   obj.sum = reqObj.sum
 // }
-var key = firebase.database().ref('customerAccess').push().key
-obj.key = key
-obj.realObjectKey = reqObj.key
-firebase.database().ref('customerAccess').child(key).set(obj)
-firebase.database().ref('partyList'+this.state.user).child(reqObj.key).set(reqObj)
-// this.state.customerAccessList.push(obj)
-alert('Customer Access successfully Granted')
-// this.setState({keyWords:''}) 
-
-
-  }
-
-}
-}
 
 
 
+// getData =()=>{
+//   this.setState({status: ! this.state.status})
+// }
 
 
-getList=()=>{
-this.setState({listStatus:true})
-}
+
+// changeHandler=(e)=>{
+// this.setState({[e.target.name]: e.target.value  })
 
 
- editAccess=(i)=>{
-
-var editPromise = new Promise((res,rej)=>{
-
-  var reqObj = this.state.customerAccessList[i]
-  var key = this.state.customerAccessList[i].key
+// }
   
-  var editPassword = prompt('Please edit Passwordetc',reqObj.keyWords)
-  if(editPassword === null){
-    editPassword = reqObj.keyWords
-  }
+
+// allowAccess = ()=>{
+
+
+//  var alreadyPartyExist = this.state.customerAccessList.find(  (o)=>{return o.partyName === document.getElementById('selected_save2').value}  )
+//  var alreadyKeyExist = this.state.customerAccessList.find(  (o)=>{return o.keyWords === this.state.keyWords}  )
+ 
+ 
+// if(alreadyPartyExist){
+//   alert('Alread Access alowed to the selected Account')
+// }else{
+
+ 
+//   if(alreadyKeyExist){
+//     alert('Password already exist, Plz change your Password')
+//   }else{
+
+
+// var obj = []
+// obj.keyWords = this.state.keyWords
+// obj.partyName = document.getElementById('selected_save2').value
+// obj.uid = this.state.user
+
+// var accountTitle = document.getElementById('selected_save2').value
+// var reqObj = this.state.partyObjects.find(  (ob)=>{return ob.partyName === accountTitle}  )
+// reqObj.keyWords = this.state.keyWords
+// // if('ledger' in reqObj){
+// // obj.ledger = reqObj.ledger
+// // obj.sum = reqObj.sum
+// // }else{
+// //   obj.sum = reqObj.sum
+// // }
+// var key = firebase.database().ref('customerAccess').push().key
+// obj.key = key
+// obj.realObjectKey = reqObj.key
+// firebase.database().ref('customerAccess').child(key).set(obj)
+// firebase.database().ref('partyList'+this.state.user).child(reqObj.key).set(reqObj)
+// // this.state.customerAccessList.push(obj)
+// alert('Customer Access successfully Granted')
+// // this.setState({keyWords:''}) 
+
+
+//   }
+
+// }
+// }
+
+
+
+
+
+// getList=()=>{
+// this.setState({listStatus:true})
+// }
+
+
+//  editAccess=(i)=>{
+
+// var editPromise = new Promise((res,rej)=>{
+
+//   var reqObj = this.state.customerAccessList[i]
+//   var key = this.state.customerAccessList[i].key
   
-  reqObj.keyWords = editPassword.replace(/  +/g, ' ').trim()
+//   var editPassword = prompt('Please edit Passwordetc',reqObj.keyWords)
+//   if(editPassword === null){
+//     editPassword = reqObj.keyWords
+//   }
   
-  firebase.database().ref('customerAccess').child(reqObj.key).set(reqObj)
+//   reqObj.keyWords = editPassword.replace(/  +/g, ' ').trim()
   
-  this.state.customerAccessList.splice(i,1,reqObj)
-
-  res('edited successfully')
-
-})
-
-editPromise.then((msg)=>{
-
-  var reqObj = this.state.customerAccessList[i]
-  // var key = this.state.customerAccessList[i].key
-  // var editPassword = prompt('Please Re-enter Passwordetc',reqObj.keyWords)
-  // if(editPassword === null){
-  //   editPassword = reqObj.keyWords
-  // }
-  // reqObj.keyWords = editPassword.replace(/  +/g, ' ').trim()
-
-  var ourObject = this.state.partyObjects.find((obj)=>{return obj.partyName === reqObj.partyName})
-  ourObject.keyWords= reqObj.keyWords
+//   firebase.database().ref('customerAccess').child(reqObj.key).set(reqObj)
   
-  firebase.database().ref('partyList'+this.state.user).child(ourObject.key).set(ourObject)
-alert(msg)
+//   this.state.customerAccessList.splice(i,1,reqObj)
 
-})
+//   res('edited successfully')
+
+// })
+
+// editPromise.then((msg)=>{
+
+//   var reqObj = this.state.customerAccessList[i]
+//   // var key = this.state.customerAccessList[i].key
+//   // var editPassword = prompt('Please Re-enter Passwordetc',reqObj.keyWords)
+//   // if(editPassword === null){
+//   //   editPassword = reqObj.keyWords
+//   // }
+//   // reqObj.keyWords = editPassword.replace(/  +/g, ' ').trim()
+
+//   var ourObject = this.state.partyObjects.find((obj)=>{return obj.partyName === reqObj.partyName})
+//   ourObject.keyWords= reqObj.keyWords
+  
+//   firebase.database().ref('partyList'+this.state.user).child(ourObject.key).set(ourObject)
+// alert(msg)
+
+// })
+
+
+// }
+
+
+
+// deleteCustomerAccess=()=>{
+//   var accountDelet = prompt("write password for which you want to delete the access")
+  
+//   var reqCustomerToBeDel = this.state.customerAccessList.find(  (o)=>{return o.keyWords === accountDelet}  )
+
+//   if(reqCustomerToBeDel){
+//   var key = reqCustomerToBeDel.key
+//   firebase.database().ref('customerAccess').child(key).remove()
+
+
+//   var DelFromArray = this.state.customerAccessList.filter(  (oo)=>{return oo.key !== reqCustomerToBeDel.key}  )
+
+//     this.setState({customerAccessList:DelFromArray})
+
+
+//     alert('Deleted Successfully')
+//   }else{
+//     alert('Your Entered password is incorrect')
+//   }
+
+
+// }
 
 
 
 
-}
 
 
 
 
 
-    render(){
-        return(
+
+//     render(){
+//         return(
          
-          <div>
+//           <div>
           
-<br/><br/><br/>
-           <div  style={{textAlign:'center', marginBottom:'0px'}}><button className="waves-effect waves-dark btn" onClick={this.getData} style={{width:'80%'}}>Select Account</button> <br/>
-  <div style={{width:'80%', margin:'auto'}}> <select className='browser-default' id='selected_save2'>  {this.state.partyObjects.map(  (item,i)=>{ return <option key={i} className='browser-default'>{item.partyName}</option>}  )       }   </select> </div> <br/></div>
+// <br/><br/><br/>
+//            <div  style={{textAlign:'center', marginBottom:'0px'}}><button className="waves-effect waves-dark btn" onClick={this.getData} style={{width:'80%'}}>Select Account</button> <br/>
+//   <div style={{width:'80%', margin:'auto'}}> <select className='browser-default' id='selected_save2'>  {this.state.partyObjects.map(  (item,i)=>{ return <option key={i} className='browser-default'>{item.partyName}</option>}  )       }   </select> </div> <br/></div>
   
-  <br/>
-  <div className='container'>
-  <span style={{fontSize:'22px', color:'blue'}}> Create Password to allow access to the selected Account </span>
-  <input type='text' onChange={this.changeHandler} name='keyWords' value={this.state.keyWords} placeholder='Create Key works' />   
-  <button className="waves-effect waves-dark btn" onClick={this.allowAccess} >Allow Access</button>
+//   <br/>
+//   <div className='container'>
+//   <span style={{fontSize:'22px', color:'blue'}}> Create Password to allow access to the selected Account </span>
+//   <input type='text' onChange={this.changeHandler} name='keyWords' value={this.state.keyWords} placeholder='Create Key works' />   
+//   <button className="waves-effect waves-dark btn" onClick={this.allowAccess} >Allow Access</button>
   
-<br/><br/><br/><br/><br/>
-<span style={{fontSize:'22px', color:'blue'}}> Get List of all customers to whome you have Granted Access </span> <br/>
-  <button className="waves-effect waves-dark btn" onClick={this.getList} >Get List</button>
+// <br/><br/><br/><br/><br/>
+// <span style={{fontSize:'22px', color:'blue'}}> Get List of all customers to whome you have Granted Access </span> <br/>
+//   <button className="waves-effect waves-dark btn" onClick={this.getList} >Get List</button>
   
   
 
-  <div className={this.state.listStatus === false ? 'display' : ''}>
-  <table><thead><tr><th>Account Title</th><th>Password</th><th>Edit</th></tr></thead><tbody>{this.state.customerAccessList.map(  (item,index)=>{return <tr key={index}><td>{(index+1) + '- ' + item.partyName}</td><td>{item.keyWords}</td><td><a href='#' className="material-icons" style={{color:'green',fontSize:'15px'}} onClick={()=> this.editAccess(index)}>edit</a></td></tr>})    }</tbody></table> 
-{/* </div> */}
-</div>
+//   <div className={this.state.listStatus === false ? 'display' : ''}>
+//   <table><thead><tr><th>Account Title</th><th>Password</th><th>Edit</th></tr></thead><tbody>{this.state.customerAccessList.map(  (item,index)=>{return <tr key={index}><td>{(index+1) + '- ' + item.partyName}</td><td>{item.keyWords}</td><td><a href='#' className="material-icons" style={{color:'green',fontSize:'15px'}} onClick={()=> this.editAccess(index)}>edit</a></td></tr>})    }</tbody></table> 
+//   <br/>
+//   <button className="waves-effect waves-dark btn" onClick={this.deleteCustomerAccess} >Delete Customer Access</button>
+
+// </div>
 
 
 
-  </div>   
+//   </div>   
           
-          </div>
+//           </div>
           
-        )
-    }
+//         )
+//     }
 
 
-  }
+//   }
--- src/components/Logout.js
+++ src/components/Logout.js
@@ -8,27 +8,222 @@ import firebase from './Fire'
     constructor(){
       super();
       this.state ={
-              user:null,
-              userEmail:null
+        user:null,
+        userEmail:null,
+        partyObjects:[],
+        status:false,
+        keyWords:'',
+        customerAccessList:[],
+        listStatus:false,
+        showAccessDiv:false
       }
 
   }
 
 
 
-  componentDidMount(){
+  async componentDidMount(){
+    var dataPushPromise = new Promise( (res,rej)=>{
     var userId = firebase.auth().currentUser.uid;
     var userEmail = firebase.auth().currentUser.email
-    
     this.setState({user:userId,userEmail:userEmail})
-  }
+    res()
+    rej('Operation Failed: Data From Firebase does not push in state successfully')
+  } )
+  dataPushPromise.then(()=>{
+
+    var pushPromise = new Promise((resolve,reject)=>{
+      var obj = [];
+      firebase.database().ref('partyList'+this.state.user).on('child_added' , (data)=> { 
+        obj.push(data.val())
+      }  )
+      resolve(obj)
+      reject('Operation failed')
+    })
+    pushPromise.then((ob)=>{
+      this.setState({partyObjects:ob})
+    },(er)=>{
+      alert(er)
+    })
+
+
+  },(err)=>{
+    alert(err)
+  })
+
+
+  firebase.database().ref('customerAccess').on('child_added' , (data)=> { 
+    this.state.customerAccessList.push(data.val())
+  }  )
 
 
 
+}
+
+
+  // componentDidMount(){
+  //   var userId = firebase.auth().currentUser.uid;
+  //   var userEmail = firebase.auth().currentUser.email
+    
+  //   this.setState({user:userId,userEmail:userEmail})
+  // }
 
     Logout= ()=>{
         firebase.auth().signOut();
     }
+
+
+
+
+
+    getData =()=>{
+      this.setState({status: ! this.state.status})
+    }
+    
+    
+    
+    changeHandler=(e)=>{
+    this.setState({[e.target.name]: e.target.value  })
+    
+    
+    }
+      
+    
+    allowAccess = ()=>{
+    
+    
+     var alreadyPartyExist = this.state.customerAccessList.find(  (o)=>{return o.partyName === document.getElementById('selected_save2').value}  )
+     var alreadyKeyExist = this.state.customerAccessList.find(  (o)=>{return o.keyWords === this.state.keyWords}  )
+     
+     
+    if(alreadyPartyExist){
+      alert('Alread Access alowed to the selected Account')
+    }else{
+    
+     
+      if(alreadyKeyExist){
+        alert('Password already exist, Plz change your Password')
+      }else{
+    
+    
+    var obj = []
+    obj.keyWords = this.state.keyWords
+    obj.partyName = document.getElementById('selected_save2').value
+    obj.uid = this.state.user
+    
+    var accountTitle = document.getElementById('selected_save2').value
+    var reqObj = this.state.partyObjects.find(  (ob)=>{return ob.partyName === accountTitle}  )
+    reqObj.keyWords = this.state.keyWords
+    // if('ledger' in reqObj){
+    // obj.ledger = reqObj.ledger
+    // obj.sum = reqObj.sum
+    // }else{
+    //   obj.sum = reqObj.sum
+    // }
+    var key = firebase.database().ref('customerAccess').push().key
+    obj.key = key
+    obj.realObjectKey = reqObj.key
+    firebase.database().ref('customerAccess').child(key).set(obj)
+    firebase.database().ref('partyList'+this.state.user).child(reqObj.key).set(reqObj)
+    // this.state.customerAccessList.push(obj)
+    alert('Customer Access successfully Granted')
+    // this.setState({keyWords:''}) 
+    
+    
+      }
+    
+    }
+    }
+    
+    
+    
+    
+    
+    getList=()=>{
+    this.setState({listStatus:true})
+    }
+    
+    
+     editAccess=(i)=>{
+    
+    var editPromise = new Promise((res,rej)=>{
+    
+      var reqObj = this.state.customerAccessList[i]
+      var key = this.state.customerAccessList[i].key
+      
+      var editPassword = prompt('Please edit Passwordetc',reqObj.keyWords)
+      if(editPassword === null){
+        editPassword = reqObj.keyWords
+      }
+      
+      reqObj.keyWords = editPassword.replace(/  +/g, ' ').trim()
+      
+      firebase.database().ref('customerAccess').child(reqObj.key).set(reqObj)
+      
+      this.state.customerAccessList.splice(i,1,reqObj)
+    
+      res('edited successfully')
+    
+    })
+    
+    editPromise.then((msg)=>{
+    
+      var reqObj = this.state.customerAccessList[i]
+      // var key = this.state.customerAccessList[i].key
+      // var editPassword = prompt('Please Re-enter Passwordetc',reqObj.keyWords)
+      // if(editPassword === null){
+      //   editPassword = reqObj.keyWords
+      // }
+      // reqObj.keyWords = editPassword.replace(/  +/g, ' ').trim()
+    
+      var ourObject = this.state.partyObjects.find((obj)=>{return obj.partyName === reqObj.partyName})
+      ourObject.keyWords= reqObj.keyWords
+      
+      firebase.database().ref('partyList'+this.state.user).child(ourObject.key).set(ourObject)
+    alert(msg)
+    
+    })
+    
+    
+    }
+    
+    
+    
+    deleteCustomerAccess=()=>{
+      var accountDelet = prompt("write password for which you want to delete the access")
+      
+      var reqCustomerToBeDel = this.state.customerAccessList.find(  (o)=>{return o.keyWords === accountDelet}  )
+    
+      if(reqCustomerToBeDel){
+      var key = reqCustomerToBeDel.key
+      firebase.database().ref('customerAccess').child(key).remove()
+    
+    
+      var DelFromArray = this.state.customerAccessList.filter(  (oo)=>{return oo.key !== reqCustomerToBeDel.key}  )
+    
+        this.setState({customerAccessList:DelFromArray})
+    
+    
+        alert('Deleted Successfully')
+      }else{
+        alert('Your Entered password is incorrect')
+      }
+    
+    
+    }
+    
+
+
+
+    access=()=>{
+      this.setState({showAccessDiv:true})
+    }
+
+
+
+
+
+
 
     render(){
         return(
@@ -69,10 +264,54 @@ import firebase from './Fire'
       </div>
     {/* </div> */}
   </div>
-  
+  </div>
 
+<br/>
 
 <div className='container'>
+<button onClick={this.access}> Allow Access to customer </button>
+</div>
+<div className={this.state.showAccessDiv===false ? 'display' : ''}>
+  <br/><br/><br/>
+  <div  style={{textAlign:'center', marginBottom:'0px'}}><button className="waves-effect waves-dark btn" onClick={this.getData} style={{width:'80%'}}>Select Account</button> <br/>
+  <div style={{width:'80%', margin:'auto'}}> <select className='browser-default' id='selected_save2'>  {this.state.partyObjects.map(  (item,i)=>{ return <option key={i} className='browser-default'>{item.partyName}</option>}  )       }   </select> </div> <br/></div>
+  
+  <br/>
+  <div className='container'>
+  <span style={{fontSize:'22px', color:'blue'}}> Create Password to allow access to the selected Account </span>
+  <input type='text' onChange={this.changeHandler} name='keyWords' value={this.state.keyWords} placeholder='Create Key works' />   
+  <button className="waves-effect waves-dark btn" onClick={this.allowAccess} >Allow Access</button>
+  
+<br/><br/><br/><br/><br/>
+<span style={{fontSize:'22px', color:'blue'}}> Get List of all customers to whome you have Granted Access </span> <br/>
+  <button className="waves-effect waves-dark btn" onClick={this.getList} >Get List</button>
+  
+  
+
+  <div className={this.state.listStatus === false ? 'display' : ''}>
+  <table><thead><tr><th>Account Title</th><th>Password</th><th>Edit</th></tr></thead><tbody>{this.state.customerAccessList.map(  (item,index)=>{return <tr key={index}><td>{(index+1) + '- ' + item.partyName}</td><td>{item.keyWords}</td><td><a href='#' className="material-icons" style={{color:'green',fontSize:'15px'}} onClick={()=> this.editAccess(index)}>edit</a></td></tr>})    }</tbody></table> 
+  <br/>
+  <button className="waves-effect waves-dark btn" onClick={this.deleteCustomerAccess} >Delete Customer Access</button>
+
+</div>
+  </div>   
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+{/* <div className='container'>
   <h2 style={{textAlign:'left'}} className='headings'>Guidance to Run this Application</h2>
   <h2 style={{textAlign:'left'}} className='headings'>Accounts-Record</h2>
   <p style={{textAlign:'justify'}}>Go to the top of the screen and click on <b>Accounts-Record</b> you will see further menue of five features of accounts;
@@ -95,7 +334,7 @@ import firebase from './Fire'
   <br/> Go to the section of <b>Account Statement</b>, Select the Account and get the Ledger/Statement having all history of transactions.
   <br/> <b>5-Summary</b>
   <br/> It is the one page summary of All opened Accounts (if they have balances). You can get Quick last 50-Transaction detail by clicking on the relevant account. 
-  </p>
+  </p> */}
 
 
 
@@ -111,10 +350,10 @@ import firebase from './Fire'
   </p> */}
 
 
-</div>
+{/* </div> */}
 
 
-            </div>
+            
             </div>
         )
     }
